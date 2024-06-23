ca1b0afc9ce67f19a2f9140045a80006b8e26891
João Costa <joaocosta@joaos-mbp.lan>
2024-06-24 00:02:14 +0100

    added smart contract

---
+++ protocol/ignition/modules/deploy.ts
@@ -0,0 +1,10 @@
+import { buildModule } from "@nomicfoundation/hardhat-ignition/modules";
+
+const DeployModule = buildModule("DeployTodos", (m: any) => {
+  const todos = m.contract("Todos");
+  const mockTokens = m.contract("MockToken");
+
+  return { todos };
+});
+
+export default DeployModule;
