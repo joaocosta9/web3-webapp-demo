3262b807afaf0f3dd6be93845ca30dc01192c47e
João Costa <joaocosta@joaos-mbp.lan>
2024-06-25 11:15:21 +0100

    add generate abi with hooks
    Signed-off-by: joaocosta9 <johnydacosta@hotmail.com>

--- wagmi.config.ts
+++ wagmi.config.ts
@@ -1,8 +1,19 @@
 import { defineConfig } from "@wagmi/cli";
-import { hardhat } from "@wagmi/cli/plugins";
-
+import { hardhat, react } from "@wagmi/cli/plugins";
 export default defineConfig({
   out: "src/generated.ts",
-  contracts: [],
-  plugins: [hardhat({ project: "./protocol" })],
+  plugins: [
+    hardhat({
+      project: "./protocol",
+      deployments: {
+        Token: {
+          31337: "0xe7f1725E7734CE288F8367e1Bb143E90bb3F0512",
+        },
+        Todos: {
+          31337: "0x5FbDB2315678afecb367f032d93F642f64180aa3",
+        },
+      },
+    }),
+    react(),
+  ],
 });
