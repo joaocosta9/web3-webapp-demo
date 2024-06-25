dd9dbdfeb568542a2f56e9d59435ae4e5753488e
João Costa <joaocosta@joaos-mbp.lan>
2024-06-25 14:23:49 +0100

    change main page folder
    Signed-off-by: joaocosta9 <johnydacosta@hotmail.com>

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,11 @@
 import { ConnectButton } from "@rainbow-me/rainbowkit";
-import type { NextPage } from "next";
 import Head from "next/head";
 import styles from "../styles/Home.module.css";
+import { useAccount } from "wagmi";
 
-const Home: NextPage = () => {
+const Home = () => {
+  const { address } = useAccount();
+
   return (
     <div className={styles.container}>
       <Head>
