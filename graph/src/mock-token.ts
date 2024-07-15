fa6ce3c52c3a048cd1bc7ac4fc61a9abef4eec2c
João Costa <joaocosta@joaos-mbp.lan>
2024-07-15 21:52:07 +0100

    change file
    Signed-off-by: joaocosta9 <johnydacosta@hotmail.com>

--- graph/src/mock-token.ts
+++ graph/src/mock-token.ts
@@ -1,35 +1,46 @@
 import {
   Approval as ApprovalEvent,
-  Transfer as TransferEvent
-} from "../generated/MockToken/MockToken"
-import { Approval, Transfer } from "../generated/schema"
+  Transfer as TransferEvent,
+} from "../generated/MockToken/MockToken";
+import {
+  Approval,
+  Transfer,
+} from "../generated/schema";
 
-export function handleApproval(event: ApprovalEvent): void {
-  let entity = new Approval(
-    event.transaction.hash.concatI32(event.logIndex.toI32())
-  )
-  entity.owner = event.params.owner
-  entity.spender = event.params.spender
-  entity.value = event.params.value
+export function handleApproval(
+  event: ApprovalEvent,
+): void {
+  const entity = new Approval(
+    event.transaction.hash.concatI32(
+      event.logIndex.toI32(),
+    ),
+  );
+  entity.owner = event.params.owner;
+  entity.spender = event.params.spender;
+  entity.value = event.params.value;
 
-  entity.blockNumber = event.block.number
-  entity.blockTimestamp = event.block.timestamp
-  entity.transactionHash = event.transaction.hash
+  entity.blockNumber = event.block.number;
+  entity.blockTimestamp = event.block.timestamp;
+  entity.transactionHash = event.transaction.hash;
 
-  entity.save()
+  entity.save();
 }
 
-export function handleTransfer(event: TransferEvent): void {
-  let entity = new Transfer(
-    event.transaction.hash.concatI32(event.logIndex.toI32())
-  )
-  entity.from = event.params.from
-  entity.to = event.params.to
-  entity.value = event.params.value
+export function handleTransfer(
+  event: TransferEvent,
+): void {
+  const entity = new Transfer(
+    event.transaction.hash.concatI32(
+      event.logIndex.toI32(),
+    ),
+  );
+  entity.from = event.params.from;
+  entity.to = event.params.to;
+  entity.value = event.params.value;
 
-  entity.blockNumber = event.block.number
-  entity.blockTimestamp = event.block.timestamp
-  entity.transactionHash = event.transaction.hash
+  entity.blockNumber = event.block.number;
+  entity.blockTimestamp = event.block.timestamp;
+  entity.transactionHash = event.transaction.hash;
 
-  entity.save()
+  entity.save();
 }
