884f3931939e85ad7b2d3e6f71a2566e82bf6f81
joaocosta9 <johnydacosta@hotmail.com>
2024-07-23 05:00:00 +0100

    test sign
    Signed-off-by: joaocosta9 <johnydacosta@hotmail.com>

--- src/components/TopBar.tsx
+++ src/components/TopBar.tsx
@@ -2,6 +2,7 @@ import { ConnectButton } from "@rainbow-me/rainbowkit";
 import React from "react";
 
 export default function TopBar() {
+  console.log("test");
   return (
     <div className="flex w-full justify-center bg-gray-800 p-4">
       <ConnectButton />
